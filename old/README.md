6448cae52a918d60edf56e482e4f37a79fcfcdbc
Rendell Cale <rendellc@stud.ntnu.no>
2021-01-25 15:33:30 +0100

    remove old, update workspace

--- old/README.md
+++
@@ -1,8 +0,0 @@
-
-# Build models
-
-Models are specified in `*.urdf.xacro` files in the model folder. Compile to urdf files by running
-```
-# pip install xacro 
-xacro -o models/model.urdf models/model.urdf.xacro
-```
---
+++ ws/src/simulator/launch/simulator.launch.py
@@ -0,0 +1,33 @@
+from launch import LaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import ThisLaunchFileDir
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+    gazebo = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                get_package_share_directory("gazebo_ros") + "/launch/gazebo.launch.py"
+            )
+    )
+    
+
+    # TODO: don't hardcode path
+    modeldir = "/home/cale/thesis-code/ws/src/simulator/models/autoagri/vehicle.sdf"
+    
+    spawn_entity = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=["-entity", "vehicle", "-file", modeldir]
+    )
+
+    return LaunchDescription([
+        gazebo,
+        spawn_entity
+    ])    
+    
