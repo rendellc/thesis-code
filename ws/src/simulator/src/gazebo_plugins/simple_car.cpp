81aaa517043c035704a8dd2000a9f6f8853f2179
Rendell Cale <rendellc@stud.ntnu.no>
2021-01-28 18:37:46 +0100

    add steering and drive torques topic

--- ws/src/simulator/src/gazebo_plugins/simple_car.cpp
+++ ws/src/simulator/src/gazebo_plugins/simple_car.cpp
@@ -9,30 +9,40 @@
 #include "simulator/msg/float_array.hpp"
 
 #include <iostream>
+#include <array>
 
 namespace gazebo_plugins
 {
 class SimpleCarPrivate
 {
 public:
+    gazebo::event::ConnectionPtr update_connection_p;
     gazebo::physics::ModelPtr model_p;
     gazebo_ros::Node::SharedPtr ros_node_p;
-    std::vector<gazebo::physics::JointPtr> steer_joints;
-    std::vector<gazebo::physics::JointPtr> drive_joints;
+    std::array<gazebo::physics::JointPtr,4> steer_joints;
+    std::array<gazebo::physics::JointPtr,4> drive_joints;
     
+    rclcpp::Subscription<simulator::msg::FloatArray>::SharedPtr drive_cmd_sub_p, steer_cmd_sub_p;
     
-    rclcpp::Subscription<simulator::msg::FloatArray>::SharedPtr drive_cmd_sub_p;
-    rclcpp::Subscription<simulator::msg::FloatArray>::SharedPtr steer_cmd_sub_p;
-
-    void OnDriveCommand(simulator::msg::FloatArray::SharedPtr msg_p){
-        rcpputils::assert_true(msg_p->data.size() == drive_joints.size(), "drive commands don't match drive joints");
-
-        RCLCPP_INFO(
-            ros_node_p->get_logger(),
-            "Recieved [%.3f, %.3f, %.3f %.3f]", msg_p->data[0], msg_p->data[1], msg_p->data[2], msg_p->data[3]
-        );
-
+    void OnUpdate(const gazebo::common::UpdateInfo& info) {
+        constexpr unsigned int z_axis_index = 2;
+        for (size_t i = 0; i < steer_torques.size(); i++){
+            steer_joints[i]->SetForce(z_axis_index, steer_torques[i]);
+            drive_joints[i]->SetForce(z_axis_index, drive_torques[i]);
+        }
     }
+
+    void OnCommand(simulator::msg::FloatArray::SharedPtr msg_p){
+        rcpputils::assert_true(msg_p->drive_torques.size() == drive_joints.size(), "drive commands don't match drive joints");
+        rcpputils::assert_true(msg_p->steer_torques.size() == steer_joints.size(), "steer commands don't match steer joints");
+        
+        std::copy(msg_p->drive_torques.cbegin(), msg_p->drive_torques.cend(), drive_torques.begin());
+        std::copy(msg_p->steer_torques.cbegin(), msg_p->steer_torques.cend(), steer_torques.begin());
+    }
+
+private:
+    std::array<double,4> drive_torques = {0.0};
+    std::array<double,4> steer_torques = {0.0};
 };
 
 SimpleCar::SimpleCar()
@@ -54,24 +64,19 @@ void SimpleCar::Load(gazebo::physics::ModelPtr model_p, sdf::ElementPtr sdf_p)
         impl_p->ros_node_p->get_logger(),
         "Loading SimpleCar plugin");
     
-    impl_p->steer_joints.insert(impl_p->steer_joints.end(), {
-        model_p->GetJoint("joint_wheel_fl_base_to_wheel_fl_steer"),
-        model_p->GetJoint("joint_wheel_rl_base_to_wheel_rl_steer"),
-        model_p->GetJoint("joint_wheel_rr_base_to_wheel_rr_steer"),
-        model_p->GetJoint("joint_wheel_fr_base_to_wheel_fr_steer")
-    });
+    impl_p->steer_joints[0] = model_p->GetJoint("joint_wheel_fl_base_to_wheel_fl_steer");
+    impl_p->steer_joints[1] = model_p->GetJoint("joint_wheel_rl_base_to_wheel_rl_steer");
+    impl_p->steer_joints[2] = model_p->GetJoint("joint_wheel_rr_base_to_wheel_rr_steer");
+    impl_p->steer_joints[3] = model_p->GetJoint("joint_wheel_fr_base_to_wheel_fr_steer");
 
-    impl_p->drive_joints.insert(impl_p->drive_joints.end(), {
-        model_p->GetJoint("joint_wheel_fl_steer_to_wheel_fl_tire"),
-        model_p->GetJoint("joint_wheel_rl_steer_to_wheel_rl_tire"),
-        model_p->GetJoint("joint_wheel_rr_steer_to_wheel_rr_tire"),
-        model_p->GetJoint("joint_wheel_fr_steer_to_wheel_fr_tire")
-    });
-    
+    impl_p->drive_joints[0] = model_p->GetJoint("joint_wheel_fl_steer_to_wheel_fl_tire");
+    impl_p->drive_joints[1] = model_p->GetJoint("joint_wheel_rl_steer_to_wheel_rl_tire");
+    impl_p->drive_joints[2] = model_p->GetJoint("joint_wheel_rr_steer_to_wheel_rr_tire");
+    impl_p->drive_joints[3] = model_p->GetJoint("joint_wheel_fr_steer_to_wheel_fr_tire");
 
     impl_p->drive_cmd_sub_p = impl_p->ros_node_p->create_subscription<simulator::msg::FloatArray>(
-        "cmd_drive", qos.get_subscription_qos("cmd_drive", rclcpp::QoS(1)),
-        std::bind(&SimpleCarPrivate::OnDriveCommand, impl_p.get(), std::placeholders::_1)
+        "command_torque", qos.get_subscription_qos("command_torque", rclcpp::QoS(1)),
+        std::bind(&SimpleCarPrivate::OnCommand, impl_p.get(), std::placeholders::_1)
     );
     RCLCPP_INFO(
         impl_p->ros_node_p->get_logger(),
@@ -79,6 +84,9 @@ void SimpleCar::Load(gazebo::physics::ModelPtr model_p, sdf::ElementPtr sdf_p)
     );
 
 
+    impl_p->update_connection_p = gazebo::event::Events::ConnectWorldUpdateBegin(
+        std::bind(&SimpleCarPrivate::OnUpdate, impl_p.get(), std::placeholders::_1)
+    );
     
     RCLCPP_WARN(
         impl_p->ros_node_p->get_logger(),
