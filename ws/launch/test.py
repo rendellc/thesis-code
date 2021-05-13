c25f466d1b641165d393ccd59c8c71fd7746113a
Rendell Cale <rendellc@stud.ntnu.no>
2021-05-13 20:24:20 +0200

    make experiment runner

--- ws/launch/test.py
+++ ws/launch/test.py
@@ -1,16 +1,37 @@
 
-
-import time
-import signal
-import subprocess
-
-from report_utils.experiments import start_experiment, stop_experiment
+import asyncio
+from asyncio.tasks import sleep
+from rclpy.node import Node
+import rclpy
 
 
-exp = start_experiment(
-    ["ros2", "launch", "launch/all.launch.py", "gui:=false", "use_single_turn:=true"])
+from report_utils.experiments import Experiment
+from report_utils.experiments import ExperimentRunnerBase
 
 
-time.sleep(10)
+class ExperimentRunnerTest(ExperimentRunnerBase):
+    def __init__(self, experiment):
+        super().__init__(experiment, 1.0)
 
-stop_experiment(exp)
+    def check_if_done(self):
+        t = self.get_clock().now()
+        if (t - self._start_time).nanoseconds > 20 * 10**9:
+            self._set_done()
+
+
+experiment = Experiment(
+    ["ros2",
+     "launch",
+     "launch/all.launch.py",
+     "gui:=false",
+     "use_simple_lap:=true",
+     "bag:=true"])
+
+
+rclpy.init()
+runner = ExperimentRunnerTest(experiment)
+runner.start()
+rclpy.spin_until_future_complete(runner, runner.future)
+runner.stop()
+runner.destroy_node()
+rclpy.shutdown()
--- ws/src/report_utils/report_utils/experiments.py
+++ ws/src/report_utils/report_utils/experiments.py
@@ -1,17 +1,51 @@
+import asyncio
+from abc import abstractmethod
 import subprocess
 import signal
 
 from dataclasses import dataclass
 
+from typing import List
+
+from rclpy.node import Node
+
 
 @dataclass
 class Experiment:
-    process: subprocess.Popen
+    cmd: List[str]
+    process: subprocess.Popen = None
 
 
-def start_experiment(cmd):
-    process = subprocess.Popen(cmd, stdout=subprocess.DEVNULL)
-    return Experiment(process)
+class ExperimentRunnerBase(Node):
+    def __init__(self, experiment, check_period):
+        super().__init__("experiment_watcher")
+        self.timer_ = self.create_timer(check_period, self.check_if_done)
+        # self._done = False
+        self._start_time = self.get_clock().now()
+
+        self.future = asyncio.Future()
+        self._experiment = experiment
+
+    @abstractmethod
+    def check_if_done(self): ...
+
+    def start(self):
+        print("starting")
+        start_experiment(self._experiment)
+
+    def stop(self):
+        print("stopping")
+        stop_experiment(self._experiment)
+
+    def _set_done(self):
+        self.future.set_result(0)
+
+
+def start_experiment(experiment):
+    experiment.process = subprocess.Popen(
+        experiment.cmd, stdout=subprocess.DEVNULL)
+
+    return experiment
 
 
 def stop_experiment(experiment: Experiment):
@@ -34,3 +68,7 @@ def stop_experiment(experiment: Experiment):
             subprocess.run(["kill", str(pid)])
 
         subprocess.run(["pkill", "gzserver"])
+
+        # sometimes we need to force kill ros2
+        subprocess.run(["pkill", "-1", "ros2"])
+        subprocess.run(["pkill", "component_conta"])
